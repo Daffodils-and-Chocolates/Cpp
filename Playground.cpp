697e8eebe89b3b6629fdb9a03b07c827d100fbb3
Daffodils-and-Chocolates <email.amanmadahad@gmail.com>
2023-09-19 09:57:13 +0530

    just naother day

--- Playground.cpp
+++ Playground.cpp
@@ -2,17 +2,44 @@
 #include <string.h>
 using namespace std;
 
-int recursion(int n){
-    if(n==0) return 0;
-    else if(n==1||n==2) return 1;
-    return recursion(n-1)+recursion(n-2);
-}
 int main(){
     int n;
-    cout << "Enter a number" <<endl;
-    cin >> n;
+    cout<< "Enter size of square array : \n";
+    cin>>n;
+    char arr[n][n];
+    memset( arr, '0', sizeof(arr) );
+    for(int i=0;i<n;i++){//display co-ordinates
+        for(int j=0;j<n;j++){
+            cout<<i<<","<<j<<"  ";
+        }
+        cout<<endl;
+    }
+    //starting n-queen
+    char temp[n][n];
     for(int i=0;i<n;i++){
-        cout<<recursion(i);
+        // reset(arr,temp);
+
+    }
+    for(int i=0;i<n;i++){//display array
+        for(int j=0;j<n;j++){
+            cout<< arr[i][j]<<" ";
+        }
+        cout<<endl;
     }
     return 0; 
 }
+template <size_t row>
+void reset(char(&arr)[row][row],char(&temp)[row][row]){
+    // copy(&arr[0][0], &arr[0][0] + n * n, &temp[0][0]);
+}
+template <size_t row>
+void queenIndex(char(&temp)[row][row],int r){
+    for(int i=0;i<row;i++){
+        if(temp[r][i]!='Q') temp[r][i]='1';//horizontal
+        if(temp[i][r]!='Q') temp[i][r]='1';//vertical
+    }
+}
+// class queen{
+// public:
+     
+// };
--- Playground2.cpp
+++ Playground2.cpp
@@ -3,13 +3,23 @@
 using namespace std;
 
 int main(){
-    int t;
-    cout << "Enter number of test cases" <<endl;
-    cin >> t;
-    while(t--){
-        int a,b;
-        cin>>a>>b;
-        cout<<a*b;
+    int n;
+    cout<< "Enter size of array : \n";
+    cin>>n;
+    char arr[n][n];
+    cout << "Enter elements of array:" <<endl;
+    memset( arr, '0', sizeof(arr) );
+    cout<< "Enter co ordinates"<<endl;
+    int a,b;
+    cin>>a>>b;
+    int c = a-b;
+    //diagonals to '1'
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i+j==a+b) arr[i][j] = '1';//left diagonal
+            if(a==b&&i==j) arr[i][j] = '1';
+
+        }
     }
     return 0; 
 }
